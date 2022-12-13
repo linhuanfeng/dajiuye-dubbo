a643c8229327cffb60426355253611da1e5bff11
hf <2303814193@qq.com>
2022-12-13 22:27:27 +0800

    前后端整合

---
+++ dajiuye-mp/common/constants.js
@@ -0,0 +1,7 @@
+const API_BASE_URL = "http://localhost:88"
+const WEBSOCKET_URL = "ws://localhost:88/websocket"
+
+export {
+  API_BASE_URL,
+  WEBSOCKET_URL
+}
--- weChat_daJiuYe2/common/constants.js
+++
@@ -1,7 +0,0 @@
-const API_BASE_URL = "http://localhost:8050"
-const WEBSOCKET_URL = "ws://localhost:8050/websocket"
-
-export {
-  API_BASE_URL,
-  WEBSOCKET_URL
-}
