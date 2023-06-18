7a2cab5c1ab02ac5d1a05cbc1c9c271819c5828d
hf <2303814193@qq.com>
2023-06-18 23:07:59 +0800

    整合

--- dajiuye-manage-frontend/src/main/java/com/lhf/dajiuye/manage/service/JobService.java
+++
@@ -1,9 +0,0 @@
-package com.lhf.dajiuye.manage.service;
-
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.lhf.dajiuye.manage.entiity.Job;
-
-public interface JobService extends IService<Job> {
-
-}
